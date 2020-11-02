9c427c3d5163f120bad3390f999dd13dbf075b13
A01055306 <71281431+A01055306@users.noreply.github.com>
2020-11-02 12:58:07 -0800

    Update todoapp.sh

--- vagrant-assignment/files/todoapp.sh
+++ vagrant-assignment/files/todoapp.sh
@@ -2,5 +2,4 @@ APP_PATH=/home/todoapp/app
 
 git clone https://github.com/timoguic/ACIT4640-todo-app.git $APP_PATH
 npm --prefix $APP_PATH install $APP_PATH
-# change CHANGEME to acit4640
-sed -i 's#localhost/CHANGEME#192.168.150.30:27017/ACIT4640#' $APP_PATH/config/database.js
+sed -i 's#localhost/ACIT4640E#192.168.150.30:27017/ACIT4640#' $APP_PATH/config/database.js
